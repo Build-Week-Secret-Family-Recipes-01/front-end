fab0dc9c38d7f7d4cce2e06f88e9de2b9a2086c1
Dylan Baker <dylanmbaker@yahoo.com>
2022-01-31 12:53:55 -0600

    started work on the home page. Also moved routes to index.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Login from './components/login';
-import PageOne from './components/page_one';
-import PageTwo from './components/page_two';
-import Header from './components/header/Header';
-import Logout from './components/logout';
-import Home from './components/home';
-import Signup from './components/signup';
+import './assets/search-background.jpg';
 
-import PrivateRoute from './components/private_route';
-
-function App() {
+const App = () => {
+  const loggedIn = true;
     return (
-        <div className='App'>
-            <Router>
-                <Header />
-                
-                <Switch>
-                  <Route exact path='/' component={Home} />
-                <Route path='/login' component={Login} />
-                <Route path='/signup' component={Signup} />
-                <PrivateRoute path='/page_one' component={PageOne} />
-                <PrivateRoute path='/page_two' component={PageTwo} />
-                <Route path='/logout' component={Logout} />
-                </Switch>
-                
-            </Router>
-        </div>
+        <section className='app'>
+            {
+              loggedIn ?
+              <div>
+                <div className='search-container'>
+                  <p>Find your secret recipes.</p>
+                  <form>
+                    <input className='search-bar' type='text' placeholder='Find recipe' />
+                  </form>
+                </div>
+                <div>Content</div>
+              </div> :
+              <div>Please Log in</div>
+            }
+        </section>
     );
 }
 
