8a5e81a4fc79d018adff8603b338da3982a4d24e
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-01-30 22:07:05 -0600

    routing skeleton done

---
+++ src/components/page_one.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PageOne = () => {
+  return(
+    <h1>
+      Welcome to Page One!!!!
+      111111111111111111111
+    </h1>
+  )
+}
+
+export default PageOne;
---
+++ src/components/page_two.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PageTwo = () => {
+  return(
+    <h1>
+      Welcome to Page Two!!!!
+      22222222222222222
+    </h1>
+  )
+}
+
+export default PageTwo;
