2b57631320bcc374afb3628ebdc82710091dc93a
Dylan Baker <dylanmbaker@yahoo.com>
2022-01-31 12:06:22 -0600

    worked on the header

--- src/App.js
+++ src/App.js
@@ -1,45 +1,35 @@
-import React from "react";
-import "./App.css";
-import { Route } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Login from "./components/login";
-import PageOne from "./components/page_one";
-import PageTwo from "./components/page_two";
-import Header from "./components/header";
-import Logout from "./components/logout";
+import Login from './components/login';
+import PageOne from './components/page_one';
+import PageTwo from './components/page_two';
+import Header from './components/header/Header';
+import Logout from './components/logout';
 import Home from './components/home';
-import Signup from "./components/signup";
+import Signup from './components/signup';
 
-import PrivateRoute from "./components/private_route";
+import PrivateRoute from './components/private_route';
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-
-      <Route exact path="/">
-        <Home />
-      </Route>
-
-      <Route path="/login">
-        <Login />
-      </Route>
-
-      <Route path="/signup">
-        <Signup />
-      </Route>
-
-      <PrivateRoute path="/page_one" component={PageOne}/>
-
-      <PrivateRoute path="/page_two" component={PageTwo}/>
-
-
-      <Route path="/logout">
-        <Logout />
-      </Route>
-
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Router>
+                <Header />
+                
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                <Route path='/login' component={Login} />
+                <Route path='/signup' component={Signup} />
+                <PrivateRoute path='/page_one' component={PageOne} />
+                <PrivateRoute path='/page_two' component={PageTwo} />
+                <Route path='/logout' component={Logout} />
+                </Switch>
+                
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './Header.css';
+
 const Header = () => {
   return(
-      <div>
-          <h1>BloomTech Secret Recipes</h1>
-          <div>
+      <header>
+          <div className='header-left'>
+              <Link to='/'>BloomTech Secret Recipes</Link>
+            </div>
+          <div className='header-right'>
               <Link to="/">Home</Link><br/>
               <Link to="login">Login</Link><br/>
+              <Link to="#">Create an Account</Link><br/>
               <Link to="page_one">Page One</Link><br/>
               <Link to="page_two">Page Two</Link><br/>
               <Link to="logout">Logout</Link><br/>
           </div>
-      </div>
+      </header>
   );
 }
 
