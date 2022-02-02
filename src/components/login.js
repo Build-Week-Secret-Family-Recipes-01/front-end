cb670ca7efe944447c02c330dea1a4e9a5aa456e
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-02 01:02:20 -0600

    changed login error message

--- src/components/login.js
+++ src/components/login.js
@@ -32,7 +32,7 @@ const Login = () => {
             })
             .catch((err) => {
                 console.log(err);
-                setError({ errorMessage: 'YOU GUESSED WRONG' });
+                setError({ errorMessage: 'INVALID USERNAME/PASSWORD' });
             });
     };
 
