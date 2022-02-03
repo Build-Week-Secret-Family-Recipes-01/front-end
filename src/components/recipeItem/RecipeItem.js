d6e292ff68ee4500d007aba4477d8442ad13db89
Tim <tim.goloshchapov@gmail.com>
2022-02-03 17:56:32 -0600

    fixed token

--- src/components/recipeItem/RecipeItem.js
+++ src/components/recipeItem/RecipeItem.js
@@ -11,8 +11,8 @@ const RecipeItem = (props) => {
                 <div className='recipe-title'>
                     {recipe.title}
                 </div>
-                {/* <div className='recipe-categories'>
-                    {recipe.categories.map((item, index) => {
+                <div className='recipe-categories'>
+                    {(recipe.categories || []).map((item, index) => {
                         return <p key={index} className='rounded-text'>{item}</p>;
                     })}
                 </div>
@@ -26,17 +26,17 @@ const RecipeItem = (props) => {
                 {recipe.servings}
                 <p><strong>Ingredients:</strong></p>
                 <ul>
-                    {recipe.ingredients.map((ingredient, index) => {
+                    {(recipe.ingredients || []).map((ingredient, index) => {
                         return <li key={index}>{ingredient}</li>;
                     })}
                 </ul>
 
                 <p><strong>Instructions:</strong></p>
                 <ol>
-                    {recipe.instructions.map((instructions, index) => {
+                    {(recipe.instructions || []).map((instructions, index) => {
                         return <li key={index}>{instructions}</li>;
                     })}
-                </ol> */}
+                </ol>
             </div>
         </div>
     );
