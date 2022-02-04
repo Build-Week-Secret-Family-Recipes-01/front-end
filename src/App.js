80ac90742bf647263c9c1857349c6da2a7fcd1a1
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-03 21:12:55 -0600

    all recipes listed, search fixed, ingredients and steps rendering

--- src/App.js
+++ src/App.js
@@ -1,106 +1,88 @@
-import React, {useEffect, useState} from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import "./App.css";
 
-import RecipeItem from './components/recipeItem/RecipeItem';
-import axios from 'axios';
+import RecipeItem from "./components/recipeItem/RecipeItem";
+import axios from "axios";
 
 const App = () => {
-    const { push } = useHistory()
-    const [auth, setAuth] = useState(false);
-    const [recipes, setRecipes] = useState([]);
+  const { push } = useHistory();
+  const [auth, setAuth] = useState(false);
+  const [recipes, setRecipes] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(recipes);
 
-    useEffect(()=>{
-        if (localStorage.getItem('token')) {
-            setAuth(true)
-        } else {
-            setAuth(false)
-        }
-    }, []);
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setAuth(true);
+    } else {
+      setAuth(false);
+    }
+  }, []);
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        axios
-            .get(`https://secret-family-recipes-01.herokuapp.com/api/recipes`, { headers: { Authorization: token}})
-            .then(resp => {
-                // console.log(resp.data)
-                setRecipes(resp.data);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios
+      .get(`https://secret-family-recipes-01.herokuapp.com/api/recipes`, {
+        headers: { Authorization: token }
+      })
+      .then(res => Object.values(res.data).map((x) => x.recipe_id))
+      .then(ids => {
+        const all = ids.map(id => axios
+          .get(
+            `https://secret-family-recipes-01.herokuapp.com/api/recipes/${id}`,
+            { headers: { Authorization: token } }
+          ));
+        return Promise.all(all)
+      })
+      .then(allResponses => {
+        const allRecipes = allResponses.map(resp => resp.data);
+        console.log(allRecipes)
+        setRecipes(allRecipes)
+        setSearchTerm(allRecipes)
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
-                // const fullData = resp.data.map(item => {
-                //     let recipe = item;
-                //     axios
-                //     .get(`https://secret-family-recipes-01.herokuapp.com/api/recipes/${item.recipe_id}`)
-                //     .then(resp => {
-                //         console.log(resp.data)
-                //         recipe = resp.data;
-                //     })
-                //     .catch(err => {
-                //         console.log(err);
-                //         // push('/login')
-                //     });
-                //     return recipe;
-                // });
-                
-                // setRecipes(fullData);
+  const handleSearchChange = (e) => {
+    e.preventDefault();
+    const term = e.target.value.toLowerCase();
+    const filtered = recipes.filter((item) => {
+      return item.title.toLowerCase().includes(term) || item.categories.map(c => c.toLowerCase()).includes(term);
+    });
 
-            })
-            .catch(err => {
-                console.log(err);
-                // push('/login')
-            });
-    }, []);
+    setSearchTerm(filtered);
+  };
 
-    
-    // const [recipes, setRecipes] = useState([]);
-    const [searchTerm, setSearchTerm] = useState(recipes);
-
-    
-
-    const handleSearchChange = (e) => {
-        const term = e.target.value.toLowerCase();
-        const filtered = recipes.filter(item => {
-            return item.title.toLowerCase().includes(term);
-        });
-        
-        setSearchTerm(filtered);
-    };
-    
-
-    console.log(recipes)
-
-    return (
-        <section className='app'>
-            {auth ? (
-                <div>
-                    <div className='search-container'>
-                        <p>Find your secret recipes.</p>
-                        <form>
-                            <input
-                                onChange={handleSearchChange}
-                                className='search-bar'
-                                type='text'
-                                name='search'
-                                id='search'
-                                placeholder='Start typing to find recipe'
-                            />
-                        </form>
-                        <div className='tab-link'><Link to='/'>All Recipes</Link></div>
-                        <div className='tab-link'><Link to='page_one'>Your Recipes</Link></div>
-                    </div>
-                    <div className='recipe-wrapper'>
-                        {
-                            recipes.map((item, index) => {
-                                return <RecipeItem key={index} recipe={item} />;
-                            })
-                        }
-                    </div>
-                </div>
-            ) : (
-                // push('/login')
-                <div> Something about this project </div>
-            )}
-        </section>
-    );
+  return (
+    <section className="app">
+      {auth ? (
+        <div>
+          <div className="search-container">
+            <p>Find your secret recipes.</p>
+            <form>
+              <input
+                onChange={handleSearchChange}
+                className="search-bar"
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Start typing to find recipe"
+              />
+            </form>
+          </div>
+          <div className="recipe-wrapper">
+            {searchTerm.map((item) => {
+              return <RecipeItem key={item.recipe_id} recipe={item} />;
+            })}
+          </div>
+        </div>
+      ) : (
+        <div> Something about this project </div>
+      )}
+    </section>
+  );
 };
 
 export default App;
